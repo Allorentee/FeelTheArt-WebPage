121fe8fecef3aa9571964f063aec2f9d9bd87425
AlvaroLlorente <Allorentee@gmail.com>
2023-01-28 10:36:37 +0100

    ADD Test

---
+++ FeelTheArt-Front/src/pages/ArtistGallery/index.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react";
+import { ArtistGallery } from "./index";
+import "@testing-library/jest-dom";
+
+describe("Given Component", () => {
+  test("should be render..", () => {
+    global.fetch = jest
+      .fn()
+      .mockResolvedValue({ json: jest.fn().mockReturnValue("") });
+    render(<ArtistGallery></ArtistGallery>);
+    const element = screen.getByText(/artist/i);
+    expect(element).toBeInTheDocument();
+  });
+});
