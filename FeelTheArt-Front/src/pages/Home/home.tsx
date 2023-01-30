a08e2d1f92d753877b413df6450138f8107c86b6
AlvaroLlorente <Allorentee@gmail.com>
2023-01-30 12:35:15 +0100

    Style homepage

--- FeelTheArt-Front/src/pages/Home/home.tsx
+++ FeelTheArt-Front/src/pages/Home/home.tsx
@@ -1,12 +1,16 @@
 import { RenderMenu } from "../../core/components/menu/menu";
+import style from "./home.module.css";
 
 export const Home = () => {
   return (
     <>
-      <RenderMenu></RenderMenu>
-      <div>
+      {/* <RenderMenu></RenderMenu> */}
+      <div className={style.home__wrapper}>
         <img src="/assets/img/face-right.png" width="280px" alt="face-right" />
-        <h1>FEEL THE ART</h1>
+        <div>
+          <h1 className={style.home__title}>FEEL THE ART</h1>
+          <h2 className={style.home__title2}>Working in progress</h2>
+        </div>
         <img src="/assets/img/face-left.png" width="280px" alt="face-left" />
       </div>
     </>
